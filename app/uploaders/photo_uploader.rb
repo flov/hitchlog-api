3e6693482861ad210a1b81f2abc37a6bb4f3b3ec
Florian Vallen <florian.vallen@gmail.com>
2022-10-18 17:07:08 +0100

    📸 Imagemagick for ride

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,6 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   if Rails.env === "test"
